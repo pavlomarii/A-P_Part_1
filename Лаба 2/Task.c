0e86ac71f07095b39f4f1edb683f5c0f43f6b0cd
Pavlo <43071980+pawlomarij1@users.noreply.github.com>
2018-09-27 18:33:52 +0300

    Add files via upload

---
+++ Лаба 2/Task.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+long factorial(int);
+
+int main()
+{
+	float n, e, fact, numerator, r, suma = 0;
+	printf("n = ");
+	scanf("%f", &n);
+	do
+	{
+	    r = 3*n-3;
+		fact = factorial(r);
+		numerator = pow(n,3);
+		e = numerator/fact;
+		printf("%.4f\n", e);
+		suma = suma + e;
+		n++
+	}
+	while (e > 0.0001);
+	printf("Suma = %.4f\n", suma);
+	return 0 
+}
+
+long factorial(int n)
+{
+	int c;
+	long result = 1;
+	
+	for (c = 1; c <= n; c++)
+		result = result * c;
+	return result;
+}	
